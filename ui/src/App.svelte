24a72b1a32d03a7a2af34c1f50b45a16db483510
Mikkel <me@zerosrealm.xyz>
2022-02-17 06:23:43 +0100

    Design changes

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -47,7 +47,7 @@
         if (menuOpen) {
             document.querySelector("body").style.width = "100vw";
         } else {
-            document.querySelector("body").style.width = "calc(100vw + 5%)";
+            document.querySelector("body").style.width = "calc(100vw + 10%)";
         }
         console.log(menuOpen);
     }
@@ -63,10 +63,11 @@
 }
 
 .container {
-    margin: 0;
+    padding: 5px;
     width: 100%;
     max-width: none;
     overflow-y: auto;
+    margin-left: 1%;
 
     left: 0vw;
     position: relative;
@@ -74,8 +75,12 @@
 }
 
 nav {
-    width: max-content;
     height: 100vh;
+    background-color: #3B4252;
+    
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 }
 
 nav ul a {
@@ -84,45 +89,55 @@ nav ul a {
 }
 
 nav ul {
+    margin: 0;
     padding: 0;
-    height: 100vh;
     list-style: none;
-    background-color: #3B4252;
 }
 
 nav ul li {
     color: #fff;
-    padding: 25px 0px;
+    margin: 5px;
+    padding: 25px 10px;
     text-align: center;
+    border-radius: 5px;
     background-color: #3B4252;
-    border: 2px solid transparent;
+    /* height: 56px; */
 }
 
-nav ul li:hover {
+nav ul li:hover,
+nav ul li.active:hover {
     color: #3B4252;
     cursor: pointer;
     background-color: #EDF6F9;
-    border: 2px solid transparent;
 }
 
 nav ul li.active {
     color: #3B4252;
-    background-color: #D8DEE9;
-    border: 2px solid transparent;
+    background-color: #EDF6F9;
+}
+
+nav ul li svg {
+    margin: 0px 5px;
 }
 
 .menu {
     left: 0%;
     position: relative;
     transition: left 0.5s;
-    width: 5%
+    width: 10%
 }
 
 .menu.closed,
 .container.menu-closed {
-    left: -5%;
+    left: -10%;
 }
 
+hr {
+    height: 1px;
+    color: white;
+    opacity: 1;
+    margin: 5px;
+}
 </style>
 
 <svelte:body class:closed={!menuOpen} />
@@ -130,15 +145,30 @@ nav ul li.active {
 <div class="menu" class:closed={!menuOpen}>
     <nav>
         <ul>
-            <li style="padding: 5px;" on:click={toggleMenu}>
+            <!-- <li style="padding: 5px;" on:click={toggleMenu}>
                 <svg class="bi" width="16" height="16" fill="currentColor"><use xlink:href="css/bootstrap-icons.svg#x" /></svg>
                 Close
-            </li>
-            <a href="/"><li class:active="{currentPage == 'home'}">Home</li></a>
+            </li> -->
+            <a href="/"><li class:active="{currentPage == 'home'}">
+                <svg class="bi" width="32" height="32" fill="currentColor">
+                    <use xlink:href="css/bootstrap-icons.svg#house-fill" />
+                </svg>
+                <br>
+                Home
+            </li></a>
             <a href="/repos"><li class:active="{currentPage == 'repos'}">Repos</li></a>
             <a href="/agents"><li class:active="{currentPage == 'agents'}">Agents</li></a>
             <a href="/backups"><li class:active="{currentPage == 'backups'}">Backups</li></a>
-            <a href="/settings"><li class:active="{currentPage == 'settings'}">Settings</li></a>
+        </ul>
+        <ul>
+            <hr>
+            <a href="/settings"><li class:active="{currentPage == 'settings'}">
+                <svg class="bi" width="32" height="32" fill="currentColor">
+                    <use xlink:href="css/bootstrap-icons.svg#gear-fill" />
+                </svg>
+                <!-- <br>
+                Settings -->
+            </li></a>
         </ul>
     </nav>
 </div>
